e0e09d9467e5bfc66d3e2a97ea2c1f196c6c7289
yusmacapar <2110631170117@student.unsika.ac.id>
2024-06-16 12:53:53 +0700

    folder ajax jquery php

---
+++ eksplorasi/ajax crud/database/ajax.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Waktu pembuatan: 16 Jun 2024 pada 07.17
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ajax`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `orang`
+--
+
+CREATE TABLE `orang` (
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `orang`
+--
+
+INSERT INTO `orang` (`nama`, `alamat`) VALUES
+('Kyungsoo', 'Korea Selatan'),
+('Siti', 'Yogyakarta'),
+('Fatimah', 'Bogor'),
+('Budi', 'Bekasi'),
+('Yusma', 'Purwakarta');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ eksplorasi/ajax halaman/hal2.php
@@ -0,0 +1,3 @@
+<?php
+
+    echo "Ini adalah halaman ke-2";
